f403eca01afedd5116eab45faa7a0ab923ec0c5d
NonatoDEv <anonatoavalos@byupathway.edu>
2026-09-22 21:11:34 -0500

    week-03-model-view-controller

--- src/models/db.js
+++ src/models/db.js
@@ -13,7 +13,7 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: false, // Set to true if your database requires SSL
+    ssl: true, // Set to true if your database requires SSL
 });
 
 /**
